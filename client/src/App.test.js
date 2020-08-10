db74357499ca1ebc86256d5b32c472f48c556507
Craig bauer <craigbauer23@gmail.com>
2020-08-10 11:24:24 -0400

    added enzyme to project and added test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
+// Must render instructions
 test('renders header', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Enter your info to calculate your BMI/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+// App Component should render 1 select dom node
+describe('App', () => {
+  it('should render a <div />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('select').length).toEqual(1);
+  });
 });
